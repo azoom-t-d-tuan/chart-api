7c3d73153bbfb1ac68376d7e9796280a7f8856b4
Thai Doan Tuan <t.d.tuan@azoom.jp>
2020-12-23 18:01:53 +0700

    :tada: Add chart api

---
+++ app/Http/Controllers/Api/DataController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Models\Data;
+use Illuminate\Http\Request;
+
+class DataController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $data = Data::select('*')->get()->toArray();
+        return $data;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Models\Data  $data
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Data $data)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Models\Data  $data
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Data $data)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Models\Data  $data
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Data $data)
+    {
+        //
+    }
+}
